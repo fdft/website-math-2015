0be161096b8464a05550f8e3ad34554271f6834d
Brandon <noemail@gmail.com>
2015-05-29 17:26:31 -0700

    final version 1 push

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from google.appengine.api import mail
 from django.utils.html import strip_tags
 from django.utils.html import escape
 from lxml.html.clean import Cleaner
+import urllib2
+import urllib
 
 app = Flask(__name__,static_url_path='')
 app.config['DEBUG'] = True
@@ -16,25 +18,49 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    print('got here')
     return app.send_static_file('index.html')
 
 @app.route('/contactme', methods=['POST'])
 def emailMe():
+	"""Get form data and forward the message to me."""
 	
-#https://cloud.google.com/appengine/docs/python/tools/libraries27	
-#http://stackoverflow.com/questions/10434599/how-can-i-get-the-whole-request-post-body-in-python-with-flask
+	
+	#https://cloud.google.com/appengine/docs/python/tools/libraries27	
+	#http://stackoverflow.com/questions/10434599/how-can-i-get-the-whole-request-post-body-in-python-with-flask
 	name=request.form.get('name');
 	phone = request.form.get('phone');
 	email = request.form.get('email');
 	message=request.form.get('message');
 	cap = request.form.get('g-recaptcha-response');
 	
-	print name, phone, email, message;
-	print cap
+	#print name, phone, email, message;
+	#print cap
 	
+	
+
+	#first check the Google recaptcha
+	url = "https://www.google.com/recaptcha/api/siteverify"
+	form_fields = {
+	  "secret": "6Lc7kQcTAAAAABhFDnbFI8euTPUyApj4_B43Gy87",
+	  "response": cap,
+	}
+		
+	form_data = urllib.urlencode(form_fields)
+	try:
+		req = urllib2.Request(url, form_data)
+		response = urllib2.urlopen(req)
+		the_page = json.loads(response.read())
+		print("cap response is " + str(the_page));
+		print(the_page['success']);
+		print("what");
+		if ( the_page['success'] != True):
+			return jsonify(success=False);#return empty object		
+	except urllib2.URLError, e:
+		return jsonify(success=False); #return empty object
+		
+
+	#so the captcha is valid. Now clean the user data
 	cleaner = Cleaner()
-	
 	cleaner.javascript = True # This is True because we want to activate the javascript filter
 	cleaner.style = True      # This is True because we want to activate the styles & stylesheet filter
 	cleaner.scripts = True
@@ -47,15 +73,17 @@ def emailMe():
 	email = cleaner.clean_html(email)
 	message = cleaner.clean_html(message)
 	
+	#build the email
 	newMess = mail.EmailMessage();
 	newMess.sender ="pizzaoptimization <pizzaoptimization@gmail.com>"
 	newMess.subject = escape(strip_tags("Website Contact for tutoring:  "+ name))
 	newMess.to = "pizzaoptimization <pizzaoptimization@gmail.com>"
 	newMess.body = escape(strip_tags("Name: " + name + "\nemail: " + email + "\nphone: " + phone + "\nmessage: " + message))
-
+	
+	#send the email
 	newMess.send()	
 	
-	return jsonify(success=1);
+	return jsonify(success=True);
 	
 
 
